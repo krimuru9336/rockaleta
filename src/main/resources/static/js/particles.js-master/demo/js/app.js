2f9fe14ac76ef2cd5e4f1ccbe32920c78689333c
Kri <kritika.murugan@informatik.hs-fulda.de>
2022-04-26 00:20:30 +0200

    updated footer

--- src/main/resources/static/js/particles.js-master/demo/js/app.js
+++ src/main/resources/static/js/particles.js-master/demo/js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 45,
         "density": {
           "enable": true,
           "value_area": 800
@@ -41,7 +41,7 @@ particlesJS('particles-js',
         }
       },
       "opacity": {
-        "value": 0.5,
+        "value": 0.6,
         "random": false,
         "anim": {
           "enable": false,
@@ -64,8 +64,8 @@ particlesJS('particles-js',
         "enable": true,
         "distance": 150,
         "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
+        "opacity": 0.6,
+        "width": 2
       },
       "move": {
         "enable": true,
