1425d0bb2c8f5a44041b1b2c6895a7c1af7922ce
Kri <kritika.murugan@informatik.hs-fulda.de>
2022-08-02 23:54:24 +0200

    formatting, code optimization

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,40 +1,41 @@
-// Open the Modal
-function openModal() {
-	document.getElementById("myModal").style.display = "block";
-}
+//form validation
 
-// Close the Modal
-function closeModal() {
-	document.getElementById("myModal").style.display = "none";
-}
+(function() {
+	'use strict';
+	window
+		.addEventListener(
+			'load',
+			function() {
+				// Get the forms we want to add validation styles to
+				var forms = document
+					.getElementsByClassName('needs-validation');
+				// Loop over them and prevent submission
+				var validation = Array.prototype.filter
+					.call(
+						forms,
+						function(
+							form) {
+							form
+								.addEventListener(
+									'submit',
+									function(
+										event) {
+										if (form
+											.checkValidity() === false) {
+											event
+												.preventDefault();
+											event
+												.stopPropagation();
+										}
+										form.classList
+											.add('was-validated');
+									},
+									false);
+						});
+			}, false);
+})();
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-	showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-	showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-	var i;
-	var slides = document.getElementsByClassName("mySlides");
-	var dots = document.getElementsByClassName("demo");
-	//  var captionText = document.getElementById("caption");
-	if (n > slides.length) { slideIndex = 1 }
-	if (n < 1) { slideIndex = slides.length }
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	}
-	slides[slideIndex - 1].style.display = "block";
-	dots[slideIndex - 1].className += " active";
-	// captionText.innerHTML = dots[slideIndex-1].alt;
-}
+//open popover
+$(document).ready(function() {
+	$('[data-toggle="popover"]').popover();
+});
